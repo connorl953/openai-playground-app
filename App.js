0b4a6cb1e83fb30aa772fb9152ca2ced0bb4652d
connorl953 <connor009988@gmail.com>
2023-03-29 22:34:12 -0700

    Added activityindicator

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {ActivityIndicator, StyleSheet} from 'react-native';
 import {createDrawerNavigator, DrawerContentScrollView} from "@react-navigation/drawer";
 import {NavigationContainer} from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
@@ -22,35 +22,33 @@ const Drawer = createDrawerNavigator();
 function chatStack() {
     return (
 
-            <ApplicationProvider {...eva} theme={eva.light}>
-                <NavigationContainer>
-                    <Drawer.Navigator drawerContent={CustomDrawerContent} screenOptions={
-                        {
-                            drawerPosition: "right",
-                            drawerType: "slide",
-                            headerShown: false,
-                            swipeEnabled: false,
-                        }}
-                    >
-                        <Drawer.Screen name={"Home"} component={HomeScreen} options={{
-                            backgroundColor: "#f7f7f8",
-                        }}/>
-                    </Drawer.Navigator>
-                </NavigationContainer>
-            </ApplicationProvider>
+        <ApplicationProvider {...eva} theme={eva.light}>
+
+            <NavigationContainer>
+                <Drawer.Navigator drawerContent={CustomDrawerContent} screenOptions={
+                    {
+                        drawerPosition: "right",
+                        drawerType: "slide",
+                        headerShown: false,
+                        swipeEnabled: false,
+                    }}
+                >
+                    <Drawer.Screen name={"Home"} component={HomeScreen} options={{
+                        backgroundColor: "#f7f7f8",
+                    }}/>
+                </Drawer.Navigator>
+            </NavigationContainer>
+        </ApplicationProvider>
 
     );
 }
 
 export default function App() {
-    return chatStack();
+    return (
+        <>
+            <ActivityIndicator color={"#000000"}/>
+            {chatStack()}
+        </>
+    );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
